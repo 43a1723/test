7290eb11aa369e0e4f50bd93ee19bd4548c57f4f
43a1723 <hai1723notgay@gmail.com>
2024-06-08 10:39:38 +0700

    Update vm.ps1

--- vm.ps1
+++ vm.ps1
@@ -1,1 +1,10 @@
-if ((Invoke-RestMethod 'https://ip-api.com/line/?fields=hosting' -Method Get) -eq 'True') { exit }
+# Lấy thông tin hệ thống
+$computerSystem = Get-WmiObject -Class Win32_ComputerSystem
+
+# Kiểm tra loại máy
+if ($computerSystem.DomainRole -eq 1 -or $computerSystem.DomainRole -eq 2) {
+    Write-Host "This is a server. Exiting..."
+    exit
+} else {
+    Write-Host "This is not a server."
+}
