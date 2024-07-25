79a03ef9e2436a26fa82db38d945be791bf8ee4e
43a1723 <hai1723notgay@gmail.com>
2024-07-25 08:18:54 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,18 +1,6 @@
 
 
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class Win32 {
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetConsoleWindow();
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-}
-"@
-$consolePtr = [Win32]::GetConsoleWindow()
-# 0 = Hide the console, 5 = Show the console
-[Win32]::ShowWindow($consolePtr, 0)
+
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
