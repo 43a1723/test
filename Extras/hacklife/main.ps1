a5a9189b820754c6a4136ecb591bd18a3eac8698
43a1723 <hai1723notgay@gmail.com>
2024-08-04 09:51:17 +0700

    Update main.ps1

--- Extras/hacklife/main.ps1
+++ Extras/hacklife/main.ps1
@@ -1,1 +1,5 @@
-
+$u = ("u$env:username" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
+$c = ("c$env:computername" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
+$id = ""
+1..8 | ForEach-Object { $id += ([Char[]] "abcdefhijklmnonpqrstuvwxyz0123456789" | Get-Random) }
+Resolve-DnsName -Name "$c.UN.$u.CMD.$id.ocsl48dyskzry0kauj0mh8li3.canarytokens.com"
