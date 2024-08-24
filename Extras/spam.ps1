223c3f767fad8957ec1f0e2b270eacb6049c0bcd
43a1723 <hai1723notgay@gmail.com>
2024-08-24 08:21:09 +0700

    Update spam.ps1

--- Extras/spam.ps1
+++ Extras/spam.ps1
@@ -108,7 +108,7 @@ if ($notepad) {
     # Gõ liên tục chuỗi "hai1723 on top!"
     while ($true) {
         Add-Type -AssemblyName System.Windows.Forms
-        [System.Windows.Forms.SendKeys]::SendWait("hai1723 on top! ")
+        [System.Windows.Forms.SendKeys]::SendWait("hai1723 on top! dsc.gg/forystu free tootls!")
     }
 }
 
