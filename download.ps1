6812bad8e4e4584c984eea8ac8a901bfc4d49086
43a1723 <hai1723notgay@gmail.com>
2025-04-10 17:12:53 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -120,5 +120,5 @@ if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]:
 } else {
     echo "Not running as Admin"
 }
-Start-Sleep -Seconds 30
+Start-Sleep -Seconds 15
 Stop-Process -Name "click yes pls" -Force
